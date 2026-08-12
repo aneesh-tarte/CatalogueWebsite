7cbae65b545e67ab7a05d468b1081d3d88f52076
Aneesh Tarte <aneesh.tarte@gmail.com>
2026-08-12 10:42:56 +0530

    fix: Add new frontend URL to CORS whitelist

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors({
     origin: [
         'https://catalogue-website-frontend.vercel.app', 
+        'https://catalogue-website-ten.vercel.app',
         'http://localhost:3000', 
         'http://localhost:5500',
         'http://127.0.0.1:5500'
